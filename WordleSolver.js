7f2ce6a671e8eb70abcac88c33bfb8c3141399cf
German González <german.gonzo@gmail.com>
2022-04-15 08:21:00 -0500

    Rome code formatter and improvements

--- WordleSolver.js
+++ WordleSolver.js
@@ -1,72 +1,92 @@
-module.exports = class WordleSolver {
-
+module.exports =
+  class WordleSolver {
     greenList = ["", "", "", "", ""];
     yellowList = ["", "", "", "", ""];
-    discarded = new Set();
+    discarded = ["", "", "", "", ""];
     log = [];
-    dictionary = []
+    dictionary = [];
     frequency = {};
 
-
     constructor(dictionary) {
-        this.dictionary = dictionary;
-        this.frequency = this.dictionary.reduce((p, w) => {
-            w.split("").forEach((c,i) => {
-                if (!p[i]) {
-                    p[i] = {[c]: 0};
-                }
-                if (!p[i][c]) {
+      this.dictionary = dictionary;
+      this.frequency =
+        this.dictionary.reduce(
+          (p, w) => {
+            w
+              .split("")
+              .forEach(
+                (c, i) => {
+                  if (!p[i]) {
+                    p[i] = { [c]: 0 };
+                  }
+                  if (!p[i][c]) {
                     p[i][c] = 0;
-                }
-                ++p[i][c]
-            });
+                  }
+                  ++p[i][c];
+                },
+              );
             return p;
-        }, {});
+          },
+          {},
+        );
+    }
+    uniq(arr) {
+      return Array.from(new Set(arr));
+    }
+    uniqLetters(str) {
+      return this.uniq(str.split("")).join("");
     }
 
-
     buildSearchRegex() {
-        const all = Array.from(new Set(this.greenList.concat(this.yellowList))).flatMap(v => v.split(""));
-        const excluded = Array.from(this.discarded).join('');
-        const regex = all.filter(v => v !== "").reduce((p, l) => p + `(?=(?:[^${l}]*${l})+[^${l}]*$)`, "");
-        const final = this.yellowList.reduce((p, c, i) => p += this.greenList[i] || (c ? `[^${c}]` : '.'), '');
+      const all = this.uniq(this.greenList.concat(this.yellowList)).flatMap((v) => v.split(""));
+      const regex = all.filter((v) => v !== "").reduce((p, l) => p + `(?=(?:[^${l}]*${l})+[^${l}]*$)`, "");
+      const final = this.discarded.reduce(
+        (p, c, i) => p += this.greenList[i] || (c ? `[^${this.uniqLetters(c)}]` : "."),
+        "",
+      );
 
-        return new RegExp(`${regex}^(?=[${all.join("")}\\w]+)(?=[^${excluded}]{5})${final}$`, 'gi');
+      return new RegExp(`${regex}^${final}$`, "i");
     }
 
     score(results) {
-        return results.map(word => ({
-            word,
-            score:  word.split("").reduce((p, c, i) => p += this.frequency[i][c] || 0, 0)
-        })).sort((a,b)=>b.score - a.score);
+      return results.map(
+        (word) => ({ word, score: word.split("").reduce((p, c, i) => p += this.frequency[i][c] || 0, 0) }),
+      ).sort((a, b) => b.score - a.score);
     }
 
     solve(word, green, yellow) {
-
-        this.log.push({
-            word,
-            green,
-            yellow
-        });
-        green.forEach((g,i)=> {
-            if (g!=="") {
-                this.greenList[i] = g;
+      this.log.push({ word, green, yellow });
+      green.forEach(
+        (g, i) => {
+          if (g !== "") {
+            this.greenList[i] = g;
+          }
+        },
+      );
+      yellow.forEach(
+        (y, i) => {
+          if (y !== "") {
+            this.yellowList[i] += y;
+            this.discarded[i] += y;
+          }
+        },
+      );
+      word
+        .split("")
+        .forEach(
+          (w, i) => {
+            const yflat = this.yellowList.flatMap((y) => y.split(""));
+            if (!this.greenList[i] && yflat.indexOf(w) < 0) {
+              this.discarded = this.discarded.map((d) => d + w);
             }
-        })
-        yellow.forEach((y,i) => {
-            if (y!=="") {
-                this.yellowList[i] += y;
-            }
-        });
-        const re = new RegExp(`[^${green}${yellow}]`, 'gi');
-        const toDiscard =  word.match(re) || [];
-        toDiscard.forEach(d => this.discarded.add(d));
-        const searchRe = this.buildSearchRegex();
-        // console.debug(searchRe);
-        return this.score(this.dictionary.filter(d => searchRe.exec(d)));
+          },
+        );
 
+      const searchRe = this.buildSearchRegex();
+      console.debug(searchRe);
+      return this.score(this.dictionary.filter((d) => searchRe.test(d)));
     }
-    isSolved() { 
-        return this.greenList.filter(v=>v!=="").length === 5;
+    isSolved() {
+      return this.greenList.filter((v) => v !== "").length === 5;
     }
-}
+  };
--- wordle_solver.js
+++ wordle_solver.js
@@ -1,103 +1,82 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const yargs = require('yargs/yargs')
-const prompt = require('prompt');
-const WordleSolver = require('./WordleSolver');
-const WordleInput = require('./WordleInput');
+
+const fs = require("fs");
+const yargs = require("yargs/yargs");
+const prompt = require("prompt");
+const WordleSolver = require("./WordleSolver");
+const WordleInput = require("./WordleInput");
 var path = process.cwd();
-
-
-
 
 prompt.start();
 
-
 async function run() {
-    var argv = require('yargs/yargs')(process.argv.slice(2))
-    .default({ n : 1, f : path + "/wordle_es_notildes.txt" })
-    .argv;
-    const wordles = [];
-    const buffer = fs.readFileSync(argv.f);
-    const data = buffer.toString().split("\n");
-    const numWordles = argv.n;
+  var argv = require("yargs/yargs")(process.argv.slice(2)).default({ n: 1, f: path + "/wordle_es_notildes.txt" }).argv;
+  const wordles = [];
+  const buffer = fs.readFileSync(argv.f);
+  const data = buffer.toString().split("\n");
+  const numWordles = argv.n;
 
-    for (let i = 0; i < numWordles; i++) {
-        wordles.push(new WordleSolver(data));
-    }
+  for (let i = 0; i < numWordles; i++) {
+    wordles.push(new WordleSolver(data));
+  }
 
-    console.log('(CTRL + C to quit)');
+  console.log("(CTRL + C to quit)");
 
-    while (true) {
-        try {
+  while (true) {
+    try {
+      let { word } = await prompt.get({ properties: { word: { required: true } } });
+      let results = [];
+      minRanking = Infinity;
+      wordleIndex = -1;
 
-            let {
-                word
-            } = await prompt.get({
-                properties: {
-                    word: {
-                        required: true
-                    }
-                }
-            });
-            let results = []
-            minResults = Infinity;
-            wordleIndex = -1;
+      for (let i = 0; i < numWordles; i++) {
+        const ws = wordles[i];
+        if (!ws.isSolved()) {
+          console.log(`Wordle #${i + 1}`);
+          let input = await WordleInput(word);
+          const { green, yellow } = input.reduce(
+            (o, { color, letter }) => {
+              o.green.push(color === "green" ? letter : "");
+              o.yellow.push(color === "yellow" ? letter : "");
+              return o;
+            },
+            { green: [], yellow: [] },
+          );
 
-            for (let i = 0; i < numWordles; i++) {
-                const ws = wordles[i];
-                if (!ws.isSolved()) {
-                    console.log(`Wordle #${i+1}`);
-                    let input = await WordleInput(word);
-                    const {
-                        green,
-                        yellow
-                    } = input.reduce((o, {
-                        color,
-                        letter
-                    }) => {
-                        o.green.push(color === "green" ? letter : "");
-                        o.yellow.push(color === "yellow" ? letter : "");
-                        return o;
-                    }, {
-                        green: [],
-                        yellow: []
-                    });
-
-
-                    r = ws.solve(word, green, yellow);
-                    if (!ws.isSolved()) {
-                        if (r.length && r.length < minResults) {
-                            minResults = r.length
-                            wordleIndex = i;
-                            results = r;
-                        }
-                    } else {
-                        console.log(`Wordle #${i+1} SOLVED ${ws.greenList.join('')}`);
-                    }
-                } else {
-                    console.log(`Wordle #${i+1} SOLVED "${ws.greenList.join('')}"`);
-                }
-
-
+          r = ws.solve(word, green, yellow);
+          if (!ws.isSolved()) {
+            //wranking
+            const wranking = (r && r.length) / (ws.greenList.filter(w => w !== "").length || 1)
+            if (wranking && wranking < minRanking) {
+              minRanking = wranking;
+              wordleIndex = i;
+              results = r;
             }
-            if (wordleIndex < 0) {
-                break;
-            }
-            console.log(`Results for wordle #${wordleIndex+1}`);
-            console.log('Matches', results.length);
-            console.log(results.slice(0, 20));
-
-
-        } catch (e) {
-            console.log('Closing...', e);
-            process.exit();
+          } else {
+            console.log(`Wordle #${i + 1} SOLVED ${ws.greenList.join("")}`);
+          }
+        } else {
+          console.log(`Wordle #${i + 1} SOLVED "${ws.greenList.join("")}"`);
         }
-
+      }
+      if (wordleIndex < 0) {
+        break;
+      }
+      console.log(`Results for wordle #${wordleIndex + 1}`);
+      console.log("Matches", results.length);
+      console.log(results.slice(0, 20));
+    } catch (e) {
+      console.log("Closing...", e);
+      process.exit();
     }
+  }
 }
 
-process.on('SIGINT', function () {
+process.on(
+  "SIGINT",
+  function () {
     process.exit();
-});
+  },
+);
 
 run();
